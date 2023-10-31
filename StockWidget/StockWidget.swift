a863201c3ae916e105806327ec77b39449580a2d
Jeyden Johnson <Jeyden.Johnson-1@student.uts.edu.au>
2023-11-01 00:16:08 +1100

    added preview for widget

--- StockWidget/StockWidget.swift
+++ StockWidget/StockWidget.swift
@@ -27,17 +27,17 @@ struct StockWidget: Widget {
 struct Provider: IntentTimelineProvider {
     func placeholder(in context: Context) -> StockWidgetEntry {
         let placeholderMessage = "No stock takes available"
-        let placeholderStockTake = StockTake()
-        placeholderStockTake.stock_description = placeholderMessage
+//        let placeholderStockTake = StockTake()
+//        placeholderStockTake.stock_description = placeholderMessage
 
-        return StockWidgetEntry(date: Date(), stockTake: placeholderStockTake)
+        return StockWidgetEntry(date: Date(), stockTake: nil)
     }
     func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (StockWidgetEntry) -> ()) {
         let placeholderMessage = "No stock takes available"
-        let placeholderStockTake = StockTake()
-        placeholderStockTake.stock_description = placeholderMessage
+//        let placeholderStockTake = StockTake()
+//        placeholderStockTake.stock_description = placeholderMessage
 
-        let entry = StockWidgetEntry(date: Date(), stockTake: placeholderStockTake)
+        let entry = StockWidgetEntry(date: Date(), stockTake: nil)
         completion(entry)
     }
 
@@ -49,11 +49,11 @@ struct Provider: IntentTimelineProvider {
             print("Fetched \(stockTakes.count) stock takes.")
             if stockTakes.isEmpty {
                             // If there are no stock takes, add a placeholder entry
-                            let placeholderStockTake = StockTake()
-                            let entry = StockWidgetEntry(date: Date(), stockTake: placeholderStockTake)
+//                            let placeholderStockTake = nil
+                            let entry = StockWidgetEntry(date: Date(), stockTake: nil)
                             entries.append(entry)
                         }
-                if let mostRecentStockTake = stockTakes.first {
+            else if let mostRecentStockTake = stockTakes.first {
                     let entry = StockWidgetEntry(date: Date(), stockTake: mostRecentStockTake)
                     entries.append(entry)
                     print(entry)
@@ -69,31 +69,59 @@ struct Provider: IntentTimelineProvider {
 
 struct StockWidgetEntry: TimelineEntry {
     let date: Date
-    let stockTake: StockTake
+    let stockTake: StockTake?
 }
 
 struct StockWidgetEntryView : View {
     var entry: StockWidgetEntry
+    
+    init(entry: StockWidgetEntry) {
+        self.entry = entry
+        print("entry", entry)
+    }
 
     var body: some View {
         
-        if entry.stockTake.status == nil {
-                    Text("No stock takes available")
-        } else {
-            VStack {
-                Text("Most Recent StockTake")
-                    .font(.headline)
-                Text("Status: \(entry.stockTake.status ?? "")")
-                Text("Description: \(entry.stockTake.stock_description ?? "")")
+        if let stock = entry.stockTake, let item = stock.item {
+            if stock.status == nil {
+                Text("No stock takes available")
+            } else {
+                VStack(alignment: .leading) {
+                    Text("\(item.name ?? "N/A")").font(.headline)
+                    Text("\(stock.inventory_from) -> \(stock.inventory_to)")
+                    Text("Status: \(stock.status ?? "")")
+                    Text("Description: \(stock.stock_description ?? "")")
+                }
             }
         }
+//        if let stock = entry.stockTake, let status = stock.status {
+//            Text("Yeah there is stock")
+//            Text(String(describing: stock))
+////            Text(stock.status ?? "No status")
+//        } else {
+//            Text("sds")
+//        }
     }
 }
 
 struct StockWidget_Previews: PreviewProvider {
     static var previews: some View {
-        let placeholderStockTake = StockTake()
-        StockWidgetEntryView(entry: StockWidgetEntry(date: Date(), stockTake: placeholderStockTake))
+        
+        let stockTake = StockTake(context: CoreDataStorage.shared.context)
+        let item = Item(context: CoreDataStorage.shared.context)
+        item.inventory = 10
+        item.barcode = "124"
+        item.name = "Gum"
+        stockTake.status = "complete"
+        stockTake.inventory_from = 5
+        stockTake.inventory_to = 10
+        stockTake.stock_description = "13132"
+        stockTake.created_at = Date()
+        stockTake.updated_at = Date()
+        stockTake.item = item
+        CoreDataStorage.shared.saveContext()
+        
+        return StockWidgetEntryView(entry: StockWidgetEntry(date: Date(), stockTake: stockTake))
             .previewContext(WidgetPreviewContext(family: .systemSmall))
     }
 }
